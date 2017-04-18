01080bb3235eaf8d1b08d74e86e780700a3bc172
Darren Brain <darren.brain@gmail.com>
2017-04-18 16:25:51 +0000

    required functions work - add check for no file upload

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
 const express = require("express");
+const multer = require("multer");
 
 var app = express();
+var upload = multer();
+
+app.get("/", express.static("."));
+app.post("/", upload.single("uploadfile"), (req,res, next) => {
+    if (req.file) {
+        console.log(req.file.size);
+        res.end(JSON.stringify({filename: req.file.originalname, size: req.file.size}))
+    }
+    else {
+        res.end(JSON.stringify({error: "no file chosen"}))
+    }
+})
 
 app.listen(process.env.PORT || 3000 || 8080);
